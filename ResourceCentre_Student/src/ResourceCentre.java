0bfc57a61ed3cd87406e5189663c28d615d5ef14
CHAN ZHI YANG <21025795@myrp.edu.sg>
2023-07-12 16:39:58 +0800

    Done by Zhi Yang

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -151,13 +151,26 @@ public class ResourceCentre {
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
 		// write your code here, Zhi yang
+		for (int i = 0; i < chromebookList.size(); i++) {
+			if (chromebookList.get(i).getIsAvailable()) {
+				output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
+						chromebookList.get(i).getDescription(), 
+					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
+					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
+			}
+		}
 		return output;
+		
 	}
 
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here, Zhi yang
-		String output = retrieveAllChromebook(chromebookList);
+		ResourceCentre.setHeader("CHROMEBOOK LIST");
+		String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION",
+				"AVAILABLE", "DUE DATE","OPERATING SYSTEM");
+		 output += retrieveAllChromebook(chromebookList);	
 		System.out.println(output);
+
 	}
 
 	// ================================= Option 2 Add an item (CRUD - Create)
--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -129,6 +129,40 @@ public class ResourceCentreTest {
 	public void testRetrieveAllChromebook() {
 		// fail("Not yet implemented");
 		// write your code here, Zhi Yang
+		//Test Case 1
+		// Test if Item list is not null and empty
+		assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
+		assertEquals("Test that the Chromebook arraylist is empty.", 0, chromebookList.size());
+		// Attempt to retrieve the Chromebooks 
+		String allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
+		String testOutput = "";
+		// Test if the output is empty
+		assertEquals("Test that nothing is displayed", testOutput, allChromebook);
+
+		//Test Case 2
+		ResourceCentre.addChromebook(chromebookList, cb1);
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		// Test that the list is not empty
+		assertEquals("Test that Chromebook arraylist size is 2.", 2, chromebookList.size());
+		// Attempt to retrieve the Chromebooks 
+		allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n","CB0011", "My Google Chromebook 1st", "Yes", "", "Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n","CB0012", "SAMSUNG Chromebook 4+", "Yes", "", "Win 10");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allChromebook);
+
+		//Test Case 3
+		cc3.setIsAvailable(false);
+		ResourceCentre.addChromebook(chromebookList, cb3);
+		assertEquals("Test that Chromebook arraylist size is 2.", 3, chromebookList.size());
+		assertFalse("Test that the last item in the arraylist is not available", camcorderList.get(2).getIsAvailable());
+		// Attempt to retrieve the Chromebooks 
+		allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n","CB0011", "My Google Chromebook 1st", "Yes", "", "Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n","CB0012", "SAMSUNG Chromebook 4+", "Yes", "", "Win 10");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allChromebook);
+
 	}
 
 	@Test
